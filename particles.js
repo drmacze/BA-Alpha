7ac27f128516d63b946de8b9ca29f020d75c6f5e
Damcza Dev <drma801@gmail.com>
2025-07-03 10:51:59 +0700

    Update particles.js

--- particles.js
+++ particles.js
@@ -1,10 +1,8 @@
-
 const canvas = document.getElementById("particles");
 const ctx = canvas.getContext("2d");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-const particlesArray = [];
 class Particle {
   constructor() {
     this.x = Math.random() * canvas.width;
@@ -13,24 +11,29 @@ class Particle {
     this.speedX = Math.random() * 1 - 0.5;
     this.speedY = Math.random() * 1 - 0.5;
   }
+
   update() {
     this.x += this.speedX;
     this.y += this.speedY;
     if (this.size > 0.2) this.size -= 0.01;
+    if (this.size < 0.2) this.size = Math.random() * 2 + 0.5;
+    if (this.x < 0 || this.x > canvas.width) this.x = Math.random() * canvas.width;
+    if (this.y < 0 || this.y > canvas.height) this.y = Math.random() * canvas.height;
   }
+
   draw() {
-    ctx.fillStyle = "rgba(255,215,100,0.6)";
+    ctx.fillStyle = "rgba(255,160,40,0.8)";
     ctx.beginPath();
     ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
     ctx.fill();
   }
 }
 
-function init() {
-  for (let i = 0; i < 120; i++) {
-    particlesArray.push(new Particle());
-  }
+const particlesArray = [];
+for (let i = 0; i < 120; i++) {
+  particlesArray.push(new Particle());
 }
+
 function animate() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   particlesArray.forEach(p => {
@@ -39,5 +42,4 @@ function animate() {
   });
   requestAnimationFrame(animate);
 }
-init();
 animate();
