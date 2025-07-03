9470b898f6d230266f17bce141f953ce18bafca2
Damcza Dev <drma801@gmail.com>
2025-07-03 09:57:16 +0700

    particles.js

--- particles.js
+++ particles.js
@@ -9,37 +9,37 @@ class Particle {
   constructor() {
     this.x = Math.random() * canvas.width;
     this.y = Math.random() * canvas.height;
-    this.size = Math.random() * 2 + 1;
-    this.speedY = Math.random() * 1 - 0.5;
-    this.speedX = Math.random() * 1 - 0.5;
+    this.size = Math.random() * 2 + 0.5;
+    this.speedX = Math.random() * 0.6 - 0.3;
+    this.speedY = Math.random() * 0.6 - 0.3;
   }
   update() {
-    this.y += this.speedY;
     this.x += this.speedX;
-    if (this.size > 0.2) this.size -= 0.01;
+    this.y += this.speedY;
+    if (this.size > 0.2) this.size -= 0.005;
   }
   draw() {
-    ctx.fillStyle = "rgba(255, 200, 50, 0.8)";
+    ctx.fillStyle = "rgba(255, 215, 100, 0.7)";
     ctx.beginPath();
     ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
     ctx.fill();
   }
 }
 
-function init() {
-  for (let i = 0; i < 150; i++) {
+function initParticles() {
+  for (let i = 0; i < 120; i++) {
     particlesArray.push(new Particle());
   }
 }
 
-function animate() {
+function animateParticles() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   particlesArray.forEach(p => {
     p.update();
     p.draw();
   });
-  requestAnimationFrame(animate);
+  requestAnimationFrame(animateParticles);
 }
 
-init();
-animate();
+initParticles();
+animateParticles();
